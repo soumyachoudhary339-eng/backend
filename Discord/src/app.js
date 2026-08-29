5beae24c19de48e46b64f22eace222630d153a98
soumyachoudhary339-eng <soumyachoudhary339@gmail.com>
2026-08-29 20:32:50 +0530

    error fixed

--- Discord/src/app.js
+++ Discord/src/app.js
@@ -3,17 +3,17 @@ import { dbconnect } from "./config/db.js"
 import cookieParser from "cookie-parser"
 import authRouter from "./routes/auth.route.js"
 import userRouter from "./routes/auth.route.js"
-import redis  from "./config/redis.js"
-
+import redis from "./config/redis.js"
 import passport from "./config/passport.js"
+
 const app = express()
 dbconnect()
 app.use(passport.initialize());
 app.use(cookieParser())
 app.use(express.json())
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
-app.use(ApiErrorHandler)
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
+
 
 
 export default app
